df9673f687324f36b65e4ddd384dcd867f526b31
Pablo Lario Gómez <62502964+Thelario@users.noreply.github.com>
2021-10-03 19:32:02 +0200

    Removing Summaries

--- AMEBA Production Project/Assets/Scripts/UI/CanvasController.cs
+++ AMEBA Production Project/Assets/Scripts/UI/CanvasController.cs
@@ -1,8 +1,6 @@
 ﻿using UnityEngine;
 
-/// <summary>
 /// This class is added to each child object that will be controller by our canvas
-/// </summary>
 public class CanvasController : MonoBehaviour
 {
     public CanvasType canvasType;
